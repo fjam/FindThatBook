7fba7ac78b07507782f08afb8eb1d3cc8703b6ad
fjam <fahad.jameel02@gmail.com>
2026-07-13 00:56:15 -0400

    undo partial ssl issue fix

--- FindThatBook.Frontend/app.js
+++ FindThatBook.Frontend/app.js
@@ -1,6 +1,6 @@
 // API Configuration
 //const apiUrl = 'http://localhost:5091'; //Development
-const apiUrl = "http://findthatbook.runasp.net";
+const apiUrl = "https://findthatbook.runasp.net";
 
 // DOM Elements
 const searchForm = document.getElementById('searchForm');
